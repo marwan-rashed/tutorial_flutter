ad08462047a503e20c54402f9670ba0f06b25fb3
Marwan Rashed <marwanrashed300@gmail.com>
2022-02-12 13:47:15 +0200

    Add catalog basic UI

--- lib/catalog.dart
+++ lib/catalog.dart
@@ -6,25 +6,53 @@ import 'cart.dart';
 class Catalog extends StatelessWidget {
   const Catalog({Key? key}) : super(key: key);
 
+  static const products = ['Product 001', 'Product 002', 'Product 003'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Catalog'),
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            onPressed: () => {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Cart()),
-              )
-            },
-          ),
-        ],
-      ),
-      body: Center(child: Center()),
-    );
+        appBar: AppBar(
+          title: const Text('Catalog'),
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Cart()),
+                )
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(5),
+          child: FractionallySizedBox(
+              widthFactor: 1,
+              child: ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return FractionallySizedBox(
+                      widthFactor: 1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                                    color: Colors.blue, width: 1.0))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(products[index]),
+                            IconButton(
+                              icon: Icon(Icons.add_shopping_cart_sharp),
+                              onPressed: () => {},
+                            ),
+                          ],
+                        ),
+                      ));
+                },
+              )),
+        ));
   }
 }
